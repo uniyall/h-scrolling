7a37a5bf3d227df0aadffe71d7950a40414e3cba
uniyall <prateekkuniyal@gmail.com>
2024-02-22 03:40:41 +0530

    Secondary Scrollbar

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navigate_next = document.getElementById("navigate-next");
   const navigate_prev = document.getElementById("navigate-prev");
+  let scrollDiv = document.querySelector(".scroll-container.snaps-inline");
+  const slider = document.querySelector(".slider");
+  const navButtons = document.querySelectorAll(".navb-sec button");
+  const refBtn = document.getElementById("nav-to-1");
+  const refBtnRec = refBtn.getBoundingClientRect();
+
+
+
   navigate_next.addEventListener("click", () => {
-    let scrollDiv = document.querySelector(".scroll-container.snaps-inline");
     scrollDiv.scrollBy({
       top: 0,
       left: 1,
-      behavior: "smooth",
     });
   });
 
-
   navigate_prev.addEventListener("click", () => {
-    let scrollDiv = document.querySelector(".scroll-container.snaps-inline");
     scrollDiv.scrollBy({
       top: 0,
       left: -1,
-      behavior: "smooth",
+    });
+  });
+
+  navButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const buttonRect = button.getBoundingClientRect();
+      const sliderRect = slider.getBoundingClientRect();
+      console.log(buttonRect);
+      const left = Math.abs(buttonRect.left - refBtnRec.left);
+      console.log(left);
+      slider.style.transform = `translate(${left}px, 0)`;
     });
   });
 });
